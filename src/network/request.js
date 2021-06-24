db408e19a083abe09affe257f51b5f0c1b1868a8
zengjiachengcode <2356924146@qq.com>
2021-06-24 20:17:13 +0800

    home主页不使用better-scroll

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: "http://152.136.185.210:7878/api/m5",
+    baseURL: "",
     timeout: 5000,
     method: 'GET'
   })
